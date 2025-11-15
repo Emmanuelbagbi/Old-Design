d87aa324e0bef5878dcbb86eefac976f6cddf913
Emmanuelbagbi <emmanuelbagbi4@gmail.com>
2025-11-15 02:46:42 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Nav/Navbar";
 import Footer from "./components/Footer/Footer";
-import HomePage from "./pages/Homepage/Homepage";
+import Homepage from "./Pages/Homepage/Homepage"; // ✔
 // import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Homepage />} />
         {/* <Route path="/about" element={<AboutPage />} /> */}
         {/* catch-all route */}
         {/* <Route path="*" element={<NotFoundPage />} /> */}
